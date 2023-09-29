d9c8807487d63d513f094d008edc16291ae9f987
moonjin-kim <ace39418@gmail.com>
2023-09-29 16:02:22 +0900

    init : init comunity server

---
+++ src/main/java/com/dogfight/dogfight/DogfightApplication.java
@@ -0,0 +1,13 @@
+package com.dogfight.dogfight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogfightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogfightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dogfight/dogfight/DogfightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogfight.dogfight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogfightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
