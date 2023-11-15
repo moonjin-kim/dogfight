59562aea96ce5f786d9ab910f78b50a5db576d96
moonjin-kim <ace39418@gmail.com>
2023-11-16 00:41:10 +0900

    fix : add repository

--- src/main/java/com/dogfight/dogfight/domain/heart/HeartRepository.java
+++ src/main/java/com/dogfight/dogfight/domain/heart/HeartRepository.java
@@ -1,6 +1,8 @@
 package com.dogfight.dogfight.domain.heart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HeartRepository extends JpaRepository<Heart, Long> {
 }
