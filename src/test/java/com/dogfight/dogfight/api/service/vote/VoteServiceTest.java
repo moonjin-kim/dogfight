c1b28d8ca59f10ddf8522cb86c271e5a22a283ae
moonjin-kim <ace39418@gmail.com>
2023-11-30 02:11:06 +0900

    test : voteTest

--- src/test/java/com/dogfight/dogfight/api/service/vote/VoteServiceTest.java
+++ src/test/java/com/dogfight/dogfight/api/service/vote/VoteServiceTest.java
@@ -19,6 +19,7 @@ import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.FileInputStream;
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -52,6 +53,57 @@ class VoteServiceTest {
         userRepository.deleteAllInBatch();
         tagRepository.deleteAllInBatch();
         voteRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("1번을 투표하면 option1count의 숫자가 1 증가한다.")
+    @Test
+    void voteNonExistId() throws Exception{
+        //given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+        FileInputStream fileInputStream1 = new FileInputStream(filePath1);
+        FileInputStream fileInputStream2 = new FileInputStream(filePath2);
+        MockMultipartFile image1 = new MockMultipartFile(
+                "images",
+                fileName1 + "." + contentType1,
+                "/image/" + contentType1,
+                fileInputStream1);
+
+        MockMultipartFile image2 = new MockMultipartFile(
+                "images",
+                fileName2 + "." + contentType2,
+                "/image/" + contentType2,
+                fileInputStream2);
+
+        String writer = "testUser";
+        User user = User.builder()
+                .account("testUser")
+                .password("!test12345")
+                .nickname(writer)
+                .email("test123@gmail.com")
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+
+        BoardCreateServiceRequest request = BoardCreateServiceRequest.builder()
+                .title("축구선수 Goat는?")
+                .writer(writer)
+                .tag("스포츠")
+                .content("메시 vs 호날두")
+                .option1("메시")
+                .option1Image(image1)
+                .option2("호날두")
+                .option2Image(image2)
+                .build();
+        //when
+        BoardResponse boardResponse = boardService.create(request,registeredDateTime);
+
+        long id = boardResponse.getVote().getId();
+
+
+        assertThatThrownBy(() -> voteService.vote(2,1))
+                .isInstanceOf(NoSuchElementException.class);
+
     }
 
     @DisplayName("1번을 투표하면 option1count의 숫자가 1 증가한다.")
