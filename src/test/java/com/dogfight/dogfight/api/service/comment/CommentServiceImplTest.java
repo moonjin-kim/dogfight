fa49cc97acde2d84c831361d559896207242233f
moonjin-kim <ace39418@gmail.com>
2023-12-08 23:01:07 +0900

    test : 존재하지 않는 댓글 삭제 테스트 추가

--- src/test/java/com/dogfight/dogfight/api/service/comment/CommentServiceImplTest.java
+++ src/test/java/com/dogfight/dogfight/api/service/comment/CommentServiceImplTest.java
@@ -471,6 +471,66 @@ class CommentServiceImplTest extends IntegrationTestSupport {
         assertThat(result).isEqualTo(deleteComment);
     }
 
+    @DisplayName("댓글을 삭제할 때 없는 댓글을 삭제하면 에러가 발생한다")
+    @Test
+    void deleteCommentNoExist() throws Exception{
+        //given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+        FileInputStream fileInputStream1 = new FileInputStream(filePath1);
+        FileInputStream fileInputStream2 = new FileInputStream(filePath2);
+        MockMultipartFile image1 = new MockMultipartFile(
+                "images",
+                fileName1 + "." + contentType1,
+                "/image/" + contentType1,
+                fileInputStream1);
+
+        MockMultipartFile image2 = new MockMultipartFile(
+                "images",
+                fileName2 + "." + contentType2,
+                "/image/" + contentType2,
+                fileInputStream2);
+
+        String writer = "testUser";
+        User user = User.builder()
+                .account("testUser")
+                .password("!test12345")
+                .nickname(writer)
+                .email("test123@gmail.com")
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+
+        BoardCreateServiceRequest boardRequest = BoardCreateServiceRequest.builder()
+                .title("축구선수 Goat는?")
+                .writer(writer)
+                .tag("스포츠")
+                .content("메시 vs 호날두")
+                .option1("메시")
+                .option1Image(image1)
+                .option2("호날두")
+                .option2Image(image2)
+                .build();
+
+        BoardResponse boardResponse = boardService.create(boardRequest,registeredDateTime);
+
+        CommentServiceRequest commentRequest1 = CommentServiceRequest.builder()
+                .content("행복합니다")
+                .boardId(boardResponse.getId())
+                .nickname("testUser")
+                .password("1234")
+                .build();
+
+        Long saveCommentId = commentService.create(commentRequest1);
+
+        //when
+
+        //then
+        assertThatThrownBy(() -> commentService.delete(saveCommentId + 1, "12345"))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("존재하지 않는 댓글입니다.");
+    }
+
     @DisplayName("댓글을 삭제할 때 비밀번호를 틀리면 에러가 발생한다")
     @Test
     void deleteCommentWrongPassword() throws Exception{
