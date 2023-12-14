c85d7d950ded52ec8693d192d407d221481a615e
moonjin-kim <ace39418@gmail.com>
2023-12-14 20:24:25 +0900

    cors Error

---
+++ src/main/java/com/dogfight/dogfight/config/security/WebConfig.java
@@ -0,0 +1,19 @@
+package com.dogfight.dogfight.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","POST")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+}
